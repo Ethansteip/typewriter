066ef01b5c6579374732120685a0a70348e4419a
Ethan <ethan.steip@gmail.com>
2022-11-23 22:53:02 -0500

    final changes in adding the \n

--- typewriter.js
+++ typewriter.js
@@ -1,16 +1,6 @@
 /*
-* typewriter.js - A program that console.logs a provided sentance to make it look like
+* typewriter.js - A program that outputs a provided sentence to make it look like
 * its being typed on the computer!
-*/
- 
- 
-/*
-*
-* Function Name - and a brief description of what it does
-*
-* @param {string} argument name - information it holds
-* @param {string} argument name - information it holds
-*
 */
 
 const sentence = "hello there from lighthouse labs";
@@ -18,15 +8,19 @@ const sentence = "hello there from lighthouse labs";
 // initialize count which will be used to increment the setTimeout delay.
 let count = 0;
 
-for (const word of sentence) {
+// Loop through each index of sentence
+// I switched from using a for in, to for of loop so I could check the index of sentence to add the final "\n".
+for (const key in sentence) {
+
+  const word = sentence[key];
+  // increment the delay by +50 ms for each iteration of the loop
   count += 50;
   setTimeout(() => {
     process.stdout.write(word);
+    //check if we're at the last index and print "\n"
+    if (Number(key) === sentence.length - 1) {
+      console.log("\n");
+    }
   }, count);
-
 }
 
-
-
- 
-/* Test Cases */
